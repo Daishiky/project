fa713faddc56804c8ee506b04b85a56980710a14
seizoux <pistolamario0@gmail.com>
2023-02-13 11:38:14 +0100

    update

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-"hi"
-"how are you?"
+import django
+from django.shortcuts import make_toast
+from django.test import SimpleTestCase
+
+
+class MakeToastTests(SimpleTestCase):
+    def test_make_toast(self):
+        self.assertEqual(make_toast(), 'toast')
